beb5b919beb0b7a322bd9f5416bea8c102bb46fb
Chris Deaton <dcdeaton@ncsu.edu>
2016-03-22 15:15:38 -0500

    add bbcode

--- views/reports/promotions.php
+++ views/reports/promotions.php
@@ -64,9 +64,8 @@ foreach ($promotions->members as $member) {
                 <div class="panel-heading">Share information</div>
 
                     <pre class='well code' id='activity'
-                        onClick="$(this).selectText()"><?php echo implode(', ', $bbcodeData); ?></pre>
-
-                <div class="panel-footer text-muted">Bb-code tabular data coming soon</div>
+                        onClick="$(this).selectText()">[table]<?php foreach ($promotions->members as $member): ?>[tr][td]<?php echo $member['forum_name'] . " &raquo; " . Rank::convert(($member['rank_id']))->abbr; ?>[/td][td]<?php echo date('y-m-d', strtotime($member['last_promotion'])); ?>[/td][/tr]<?php endforeach; ?>[/table]
+                    </pre>
             </div>
 
         </div>
