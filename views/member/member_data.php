17db05af10de2a05581dd0bb5e0fb00280bd96a8
flashadvocate <flashadvocate@gmail.com>
2016-05-03 19:29:56 -0400

    change handle view

--- views/member/member_data.php
+++ views/member/member_data.php
@@ -58,8 +58,8 @@
         <?php if ($alias->isVisible): ?>
             <?php $invalid = ($alias->isInvalid) ? "<label class=\"label label-danger\" title=\"Invalid\"><i class=\"fa fa-times\"></i></label>" : null; ?>
             <a target="_blank"
-               href="<?php echo (property_exists($alias, 'url')) ? $alias->url . $alias->handle_value : "#"; ?>"
-               class="list-group-item"><?php echo $alias->name . $invalid ?> <span class='pull-right'><i
+               href="<?php echo $alias->url . $alias->handle_value; ?>"
+               class="list-group-item"><?php echo $alias->name . $invalid ?> - <?php echo $alias->handle_value ?> <span class='pull-right'><i
                         class='text-info fa fa-external-link'></i></span></a>
         <?php endif; ?>
     <?php endforeach; ?>
