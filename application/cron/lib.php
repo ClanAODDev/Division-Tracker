15a6780331412593b39b462f9992fc96e0836f5b
Chris Deaton <flashadvocate@gmail.com>
2016-06-21 14:50:31 -0400

    updates

--- application/cron/lib.php
+++ application/cron/lib.php
@@ -1,7 +1,7 @@
 <?php
 
 require 'config.php';
-require '/usr/www/aodwebhost/public/tracker/application/uagent.php';
+require '/var/www/html/tracker/application/uagent.php';
 
 function dbConnect()
 {
