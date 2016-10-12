3d1fd816a68439ec90a7bb4634857bf8da2674e8
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-12 00:46:01 -0400

    bb-code edits to BDO

--- models/division_structures/BdoDivisionStructure.php
+++ models/division_structures/BdoDivisionStructure.php
@@ -134,7 +134,7 @@ class BdoDivisionStructure
 
                 $bl_url = "[size=1]IGF: {$player->handle}[/size]";
 
-                $division_structure .= "[size=3][color={$this->platoon_pos_color}]Platoon Leader[/color]\r\n{$aod_url}\r\n{$bl_url}[/size]\r\n\r\n";
+                $division_structure .= "[size=3][color={$this->platoon_pos_color}]Platoon Leader[/color][/size]\r\n{$aod_url}\r\n{$bl_url}\r\n\r\n";
             } else {
                 $division_structure .= "[size=3][color={$this->platoon_pos_color}]Platoon Leader[/color]\r\n[color={$this->platoon_leaders_color}]TBA[/color][/size]\r\n\r\n";
             }
@@ -158,7 +158,7 @@ class BdoDivisionStructure
                         $bl_url = '[color=#ff0000]XXX[/color]';
                     }
 
-                    $division_structure .= "[size=3][color={$this->platoon_pos_color}]Squad Leader[/color]\r\n{$aod_url}\r\n{$bl_url}\r\n\r\n";
+                    $division_structure .= "[size=3][color={$this->platoon_pos_color}]Squad Leader[/color][/size]\r\n{$aod_url}\r\n{$bl_url}\r\n\r\n";
 
                     // direct recruits
                     $recruits = arrayToObject(Member::findRecruits($squad_leader->member_id, $squad_leader->platoon_id, $squad->id, true));
