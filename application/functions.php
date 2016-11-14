5e0e84e3171123ec99e2dc61452a9406a66a1990
Chris Deaton <dcdeaton@ncsu.edu>
2016-11-13 19:41:57 -0500

    add missing functions

--- application/functions.php
+++ application/functions.php
@@ -471,3 +471,13 @@ function get_headers_curl($url)
     $r = split("\n", $r);
     return $r;
 }
+
+function get_bf4db_dump($url) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
