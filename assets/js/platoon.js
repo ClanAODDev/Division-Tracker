7faa19e0263f54f669ffe9754e157ad5fc64e820
Chris Deaton <dcdeaton@ncsu.edu>
2017-02-03 09:29:11 -0500

    fix platoon links one more time

--- assets/js/platoon.js
+++ assets/js/platoon.js
@@ -1,7 +1,7 @@
     $(function() {
 
         $("#members-table tbody tr").click(function() {
-            window.location.href = "/tracker/member/" + $(this).attr('data-id');
+            window.location.href = $(this).attr('data-member-url');
         })
 
         var platoonNum = parseInt($('.platoon-number').text()),
